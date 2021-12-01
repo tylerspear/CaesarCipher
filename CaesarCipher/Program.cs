67159b23d8513770454f1771f3cdd202bbdb2141
Tyler Spear <tyler@Tylers-iMac.local>
2021-12-01 15:51:41 -0600

    broke encryption functionality out into its own method

--- CaesarCipher/Program.cs
+++ CaesarCipher/Program.cs
@@ -6,8 +6,6 @@ namespace CaesarCipher
     {
         static void Main(string[] args)
         {
-            char[] alphabet = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
-
             //get string input from the user
             Console.WriteLine("Enter a text to encrypt: ");
             string plainText = Console.ReadLine();
@@ -16,6 +14,14 @@ namespace CaesarCipher
             Console.WriteLine("Enter an encryption number: ");
             int key = Convert.ToInt32(Console.ReadLine());
 
+            Console.WriteLine(Encrypt(plainText, key));
+        }
+
+
+        static string Encrypt(string plainText, int key)
+        {
+            char[] alphabet = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
             //convert the message to an array of chars
             char[] splitText = plainText.ToCharArray();
 
@@ -23,24 +29,23 @@ namespace CaesarCipher
             char[] encryptedText = new char[splitText.Length];
 
             //loop though the plain text
-            for (int i = 0; i < plainText.Length; i++)
+            for (int i = 0; i < splitText.Length; i++)
             {
                 //find the position of each letter in the alphabet
-                char letter = plainText[i];
+                char letter = splitText[i];
                 int position = Array.IndexOf(alphabet, letter);
-
                 //rotate each letter by key amount, looping around after position 26
                 position = (position + key) % 26;
-
                 //assign new letter
                 letter = alphabet[position];
-
                 encryptedText[i] = letter;
             }
 
             //Join the array together as a string
             string finalMessage = String.Join("", encryptedText);
-            Console.WriteLine(finalMessage);
+            return finalMessage;
+
         }
+
     }
 }
